0ec8ec50f78065f74b4740eefbb7a309a3310690
Miles343 <james.potter.nz@gmail.com>
2019-08-30 21:45:28 +1200

    Favourite transportation displayed.

--- Assign2PartD/src/PartD.java
+++ Assign2PartD/src/PartD.java
@@ -1,7 +1,9 @@
+
 public class PartD {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Miles.");
 		System.out.println("My favourite colour is blue.");
+		System.out.println("My favourite mode of transportation is by plane.");
 	}
 }
